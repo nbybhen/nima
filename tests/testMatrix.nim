e16cb5a8bfa09b89aa2cff057d0afa39bf464790
Henock Dinberu <henockdinberu11@gmail.com>
2025-03-02 23:16:14 -0500

    added small test

--- tests/testMatrix.nim
+++ tests/testMatrix.nim
@@ -5,6 +5,7 @@ import nima/matrix
 suite "Matrix operations":
   var matA: Matrix[int] = newMatrix(2, 2, @[@[1, 2], @[3, 4]])
   var matB: Matrix[int] = newMatrix(2, 2, @[@[5, 6], @[7, 8]])
+  var matC: Matrix[int] = newMatrix(data = @[@[1, 2, 3], @[4, 5, 6], @[7, 8, 9]])
   var matA3x3: Matrix[int] = newMatrix(3, 3, @[@[0, 3, 5], @[5, 5, 2], @[3, 4, 3]])
   var matB3x3: Matrix[int] = newMatrix(3, 3, @[@[2, 0, -2], @[0, -1, 1], @[4, -2, 2]])
 
@@ -20,6 +21,7 @@ suite "Matrix operations":
   test "Determinant":
     check matA.determinant() == -2
     check matB.determinant() == -2
+    check matC.determinant() == 0
     check matA3x3.determinant() == -2
     check matB3x3.determinant() == -8
     check newMatrix(data = @[@[2, 1, 3, 4], @[0, -1, 2, 1], @[3, 2, 0, 5], @[-1, 3, 2, 1]]).determinant() == 35
