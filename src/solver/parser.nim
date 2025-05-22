e53cd2d2bb43f8bf11439d00473fb9fb1b5aa86f
nbybhen <henockdinberu11@gmail.com>
2025-05-22 02:57:11 -0400

    refactor: switched to nud and led funcs

--- src/solver/parser.nim
+++ src/solver/parser.nim
@@ -22,12 +22,16 @@ const bindingPower = {'+': 10, '*': 20, '=': 5}.toTable
 
 template peek(self: Parser): Token = self.src[self.current]
 
-proc advance(self: var Parser): Token =
+proc advance(self: var Parser): Token {.inline.} =
   self.current += 1
   self.src[self.current - 1]
 
+proc nud(self: var Parser): Expr {.inline.} = Expr(kind: Unary, val: self.advance())
+
+proc led(self: var Parser, left: Expr): Expr {.inline.}
+
 proc expression(self: var Parser, rbp: int): Expr =
-  var left = Expr(kind: Unary, val: self.advance())
+  var left = self.nud()
   if self.peek.kind == tkEOF:
     return left
 
@@ -35,23 +39,35 @@ proc expression(self: var Parser, rbp: int): Expr =
   case op.kind:
   of tkAdd, tkMult:
     while bindingPower[op.val] > rbp and self.peek.kind != tkEOF:
-      left = Expr(kind: Binary, left: left, op: self.advance(), right: self.expression(bindingPower[op.val]))
+      left = self.led(left)
   else:
     return left
   left
+
+proc led(self: var Parser, left: Expr): Expr {.inline.} =
+  let op = self.advance()
+  let bp = bindingPower.getOrDefault(op.val, 0);
+
+  if op.val == '=':
+    if left.kind != Unary:
+      # TODO: Handle this error if left is a Binary operation
+      discard
+    return Expr(kind: Binary, left: left, op: op, right: self.expression(bp - 1))
+  Expr(kind: Binary, left: left, op: op, right: self.expression(bp))
+
 
 proc parse*(self: var Parser): Expr =
   self.expression(0)
 
 proc cleanPrint*(node: Expr, prefix: string = "", isLeft: bool = true) =
-  const leftBar = "└──"
-  const rightBar = "├──"
+  const rightBar = "└──"
+  const leftBar = "├──"
 
   case node.kind:
   of Binary:
-    echo leftBar, node[].op
-    cleanPrint(node.right, prefix & "    ", false)
+    echo prefix, rightBar, node[].op
     cleanPrint(node.left, prefix & "    ", true)
+    cleanPrint(node.right, prefix & "    ", false)
   of Unary:
     if isLeft:
       echo prefix, leftBar, node[].val
